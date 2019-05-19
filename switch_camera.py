1224f286e1f6e7ece67747a381c89799a1655ca1
carheart <carheart@gmail.com>
2019-05-19 13:24:58 +0200

    camera configuration via input added

--- switch_camera.py
+++ switch_camera.py
@@ -4,8 +4,31 @@
 
 import os
 import json
-from farmware_tools import device
+import farmware_tools
 
-camera = os.getenv('camera', 'USB')
-switch_camera_to = 'RPI' if 'USB' in camera else 'USB'
-device.set_user_env('camera', json.dumps(switch_camera_to))
+
+#try:
+
+from farmware_tools import device
+from farmware_tools import get_config_value
+
+switch_camera_to = get_config_value('Switch Camera', 'camera_setter')
+
+switch_camera_to = switch_camera_to.strip();
+switch_camera_to = switch_camera_to.capitalize();
+
+if switch_camera_to != "RPI" or switch_camera_to != "USB":
+    device.log(message='Wrong input to switch camera provided - shd be USR or RPI', message_type='info')
+else:
+    camera = os.getenv('camera', 'USB')
+    device.set_user_env('camera', json.dumps(switch_camera_to))
+
+    # camera = os.getenv('camera', 'USB')
+    # switch_camera_to = 'RPI' if 'USB' in camera else 'USB'
+    # device.set_user_env('camera', json.dumps(switch_camera_to))
+
+
+#except Exception as error:
+#    farmware_tools.device.log(repr(error))
+
+
