ad4eb0c01f2897e674b2615a09a7509fb619b033
carheart <carheart@gmail.com>
2019-05-19 14:17:45 +0200

    typo

--- switch_camera.py
+++ switch_camera.py
@@ -8,7 +8,8 @@ import json
 from farmware_tools import device
 from farmware_tools import get_config_value
 
-switch_camera_to = get_config_value('Switch Camera', 'camera_setter')
+#switch_camera_to = get_config_value('Switch Camera', 'camera_setter')
+switch_camera_to = get_config_value(farmware_name='Switch Camera', config_name='camera_setter', value_type=str)
 switch_camera_to = switch_camera_to.strip()
 switch_camera_to = switch_camera_to.capitalize()
 
